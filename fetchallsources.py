8b81cc6d20f0bc9e31fb754fd49409e1557029b8
Sarah Chastain <schastain@gwu.edu>
2023-05-02 19:02:58 +0200

    New script grabs all fluxes from dataset for lightcurve making

---
+++ fetchallsources.py
@@ -0,0 +1,37 @@
+import numpy as np
+import psycopg2
+import datetime
+import os 
+import sys
+import glob
+import argparse    
+
+host = os.getenv('TKP_DBHOST')
+port = os.getenv('TKP_DBPORT')
+user = os.getenv('TKP_DBUSER')
+password = os.getenv('TKP_DBPASSWORD')
+database = os.getenv('TKP_DBNAME')
+if password is None:
+    conn = psycopg2.connect("dbname="+database+" user="+user+" host="+host)
+else:
+    conn = psycopg2.connect("dbname="+database+" user="+user+" password="+password+" host="+host)
+cur = conn.cursor()
+
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('dataset', type=int, help='Dataset to retrieve')
+args = parser.parse_args()
+# In order to use MJD
+start_epoch = datetime.datetime(1858, 11, 17, 00, 00, 00, 00)
+
+cur.execute("""SELECT DISTINCT assocxtrsource.runcat, image.taustart_ts, extractedsource.f_int, 
+    extractedsource.f_int_err, extractedsource.f_peak, extractedsource.f_peak_err FROM extractedsource JOIN 
+    image ON image.id=extractedsource.image JOIN assocxtrsource ON assocxtrsource.xtrsrc=extractedsource.id 
+    WHERE image.dataset=%s ;""",
+    (args.dataset,))
+fetched = cur.fetchall()
+with open(f'dataset{args.dataset}dump.csv', 'w') as f:
+    f.write("id,date,F_int,F_int_err,F_pk,F_pk_err\n")
+    for dat in fetched:
+        f.write(f'{dat[0]},{(dat[1] - start_epoch).total_seconds()/3600./24.},{dat[2]},{dat[3]},{dat[4]},{dat[5]}\n')
+
